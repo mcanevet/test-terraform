49ec99e7e6d69e41c3f2ec8aad7ef57d6d5ae9cf
Mickaël Canévet <mickael.canevet@gmail.com>
2023-05-21 18:33:33 +0200

    foo

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-local {
-    module_version = startswith(file("${path.module}/.git/HEAD"), "ref: ") ? split("/", file("${path.module}/.git/HEAD"))[-1] : file("${path.module}/.git/HEAD")
+locals {
+    module_ref = file("${path.module}/.git/HEAD")
+    module_version = startswith(local.module_ref, "ref: ") ? element(split("/", local.module_ref), length(split("/", local.module_ref)) - 1) : local.module_ref
 }
 
 output "debug" {
